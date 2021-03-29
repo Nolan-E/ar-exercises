167d826dd0fa6408731cfa817a4a790e11dba855
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-29 17:46:56 -0600

    Update exercise_6. Update employee and store class.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Austin", last_name: "Powers", hourly_rate: 50)
+@store1.employees.create(first_name: "Jasmin", last_name: "Cuevas", hourly_rate: 15)
+@store1.employees.create(first_name: "Amara", last_name: "Hebert", hourly_rate: 18)
+@store2.employees.create(first_name: "Juanita", last_name: "Cresswell", hourly_rate: 55)
+@store2.employees.create(first_name: "Kirk", last_name: "Melia", hourly_rate: 22)
+@store2.employees.create(first_name: "Fredrick", last_name: "Dale", hourly_rate: 16)
